9a698d4a6ea7f4ca7aa8d455bd41160e42f0f180
Jason Kuchynka <jason.kuchynka@gmail.com>
2012-08-07 14:40:23 -0700

    Add initial site code

---
+++ index.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * version 0.1
+ */
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <title>Git demo</title>
+    <link rel="stylesheet" href="/css/bootstrap.css" />
+    <link rel="stylesheet" href="/css/site.css" />
+    <style>
+        html, body {
+            height: 100%;
+        }
+        footer {
+            color: #666;
+            background: #222;
+            padding: 17px 0 18px 0;
+            border-top: 1px solid #000;
+        }
+        footer a {
+            color: #999;
+        }
+        footer a:hover {
+            color: #efefef;
+        }
+        .wrapper {
+            min-height: 100%;
+            height: auto !important;
+            height: 100%;
+            margin: 0 auto -63px;
+        }
+        .push {
+            height: 63px;
+        }
+        /* not required for sticky footer; just pushes hero down a bit */
+        .wrapper > .container {
+            padding-top: 60px;
+        }
+    </style>
+  </head>
+  <body>
+    <div class="wrapper">
+      <div class="container">
+        <header class="hero-unit">
+          <h1>Git Demo</h1>
+        </header>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus vel dolor nec nunc aliquam feugiat eu ac enim. Suspendisse accumsan, velit sed porta convallis, ligula enim congue lorem, in viverra magna neque ut ipsum. Aenean eget odio ipsum, eu ultrices felis. Donec nec viverra mauris. Phasellus lacinia eros libero. Maecenas dui libero, consequat a adipiscing vitae, adipiscing eu dolor. Nunc convallis pellentesque tortor non faucibus. Nulla consequat velit ac urna tempor porta. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam mi enim, pulvinar feugiat ornare eu, eleifend nec augue. Maecenas elit mauris, pretium vel rutrum nec, viverra eget purus. In nec purus leo. Sed non purus vel sem adipiscing aliquet ut vitae nunc. Vestibulum molestie iaculis rhoncus. In bibendum pharetra dui, nec auctor felis scelerisque vitae.</p>
+
+<p>Vivamus mollis luctus mi quis dictum. Duis sagittis turpis ut elit faucibus pretium. Nulla tempus, leo at tempor ultrices, ipsum justo dignissim est, vitae blandit lectus purus adipiscing sapien. Phasellus non sem ligula. Donec nec tempor urna. Curabitur fringilla, libero ut elementum ullamcorper, quam est accumsan elit, nec mollis sapien augue sit amet nisi. Quisque tristique, velit sed eleifend rutrum, dui libero laoreet orci, vel ultricies nisl urna eget mauris. Vivamus tellus velit, pulvinar quis dapibus congue, porttitor sit amet nunc. Integer a tortor in dolor iaculis aliquam. Curabitur aliquet congue turpis, quis condimentum sem pellentesque vitae. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Proin consequat elementum libero, vel semper nisi interdum sit amet. Duis dignissim vehicula dolor, a porttitor eros feugiat sed. Nunc tellus leo, consequat sed viverra nec, sodales eu diam.</p>
+
+<p>Duis malesuada luctus lorem, quis viverra velit pellentesque quis. Phasellus aliquam nulla ut metus venenatis sit amet faucibus ipsum vestibulum. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Etiam non adipiscing tortor. Proin pellentesque scelerisque nunc, eget venenatis nulla hendrerit id. Pellentesque sit amet risus sed mauris pharetra scelerisque non non sem. Pellentesque risus diam, sodales vitae pharetra a, tempus eget eros. Etiam hendrerit interdum lacus, id dictum turpis rutrum et. Sed non elit ac lectus pellentesque interdum non nec augue. Donec tincidunt faucibus enim in feugiat. In hac habitasse platea dictumst. Sed tempus viverra metus, at luctus purus pellentesque non. Aliquam non arcu arcu, dapibus auctor lorem.</p>
+
+<p>Donec a sem sit amet lorem suscipit pulvinar. Suspendisse facilisis, tortor ut tristique tempor, orci elit egestas massa, ut lobortis metus sapien at dolor. Aenean sodales dignissim malesuada. Nam id erat massa. Sed interdum justo vitae massa porttitor mollis. Pellentesque lobortis luctus neque, ut rhoncus neque dapibus non. Integer nec ipsum eros, sed egestas augue. Donec fermentum rhoncus enim, et viverra lectus facilisis quis. Aenean sed orci vel leo commodo convallis vel a orci. Pellentesque fringilla urna ut odio vestibulum semper. Sed diam lacus, interdum id adipiscing vitae, interdum ut sem. Mauris accumsan molestie erat quis tristique. Morbi semper leo non risus pellentesque vehicula. In aliquet, lacus sed feugiat mollis, dolor nulla elementum felis, sit amet tempus tellus nisl et dui.</p>
+
+<p>Fusce consectetur consequat nisi id pretium. Morbi sed nibh turpis, a dignissim mi. Quisque consequat lacus ut purus eleifend auctor varius metus mattis. Etiam eleifend felis non lectus lacinia sed fringilla lectus vestibulum. Mauris ac neque nunc, a ornare nisl. Maecenas dictum sem id tortor faucibus semper. Donec vel dolor lacus. Vestibulum at dolor id lacus sagittis vulputate sed a justo. Mauris sed feugiat nulla. In tincidunt risus a felis congue lacinia. Donec quis bibendum dolor. Donec nibh velit, mattis vitae aliquet eget, malesuada quis ipsum. Nam eget nunc magna, quis iaculis velit. Sed vitae nulla at sem rutrum faucibus. Ut sollicitudin metus vel eros tincidunt elementum. </p>
+      </div>
+      <div class="push"><!--//--></div>
+    </div>
+    <footer>
+      <div class="container">
+        <p>Git demo footer</p>
+      </div>
+    </footer>
+  </body>
+</html>
